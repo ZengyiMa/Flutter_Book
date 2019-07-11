8231e95dcc7043389071e23f59a3467f3289fbeb
mazengyi <m.zengyi@gmail.com>
2019-07-12 00:19:24 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Flutter Guide Book',
+  description: 'Learn Flutter quick',
+  base: '/Flutter_Book/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '语言', items: [
+          { text: '中文', link: '/language/chinese' },
+          { text: 'English', link: '/language/english' }
+        ]
+      },
+      { text: 'GitHub', link: 'https://google.com' },
+    ],
+    lastUpdated: true,
+  }
+}
